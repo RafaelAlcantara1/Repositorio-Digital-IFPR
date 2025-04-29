e23b21ec3a74839de9729bc2cbbe116a2a7a4be8
RafaelAlcantara1 <151959388+RafaelAlcantara1@users.noreply.github.com>
2025-04-29 11:03:33 -0300

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,16 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
 
-const sequelize = new Sequelize(
-  "repositorioteste",
-  "avnadmin",
-  "AVNS_TaG_V3h7TAOOQvnAyDN", {
-    host: "repositorioteste-repositorioteste.d.aivencloud.com",
-    dialect: 'mysql', 
-  }
-);
+const sequelize = new Sequelize('repositorioteste', 'avnadmin', 'AVNS_TaG_V3h7TAOOQvnAyDN', {
+  host: 'repositorioteste-repositorioteste.d.aivencloud.com',
+  port: 24492,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync(path.join(__dirname, './ca.pem')),
+    },
+  },
+});
 
 module.exports = sequelize;
