6314a72734daaf9a295352da6f95fb26d059739a
RafaelAlcantara1 <151959388+RafaelAlcantara1@users.noreply.github.com>
2025-06-11 20:04:26 -0300

    Update auth.js

--- frontend/src/config/auth.js
+++ frontend/src/config/auth.js
@@ -3,7 +3,7 @@ const AUTH_CONFIG = {
   // Tempo de expiração da sessão em milissegundos (24 horas)
   sessionTimeout: 24 * 60 * 60 * 1000,
   // URL base da API
-  apiUrl: 'http://localhost:3001/api'
+  apiUrl: 'https://repositorio-digital-ifpr.onrender.com'
 };
 
 export default AUTH_CONFIG; 
